5955bfc15ada9ffbdb786da4518ad8d93d7ab025
jiro.kirameki <ec2-user@ip-172-31-46-186.us-east-2.compute.internal>
2018-06-01 00:31:56 +0000

    boostrap design

--- resources/views/tasks/edit.blade.php
+++ resources/views/tasks/edit.blade.php
@@ -4,11 +4,11 @@
 
     <h1>id: {{ $task->id }} のタスク編集ページ</h1>
     <div class="row">
-        <div class="col-xs-6">
+        <div class="col-xs-12 col-md-offset-2 col-md-8 col-lg-offset-3 col-lg-6">
     {!! Form::model($task, ['route' => ['tasks.update', $task->id], 'method' => 'put']) !!}
         <div class="form-group">
         {!! Form::label('status', 'ステータス:') !!}
-        {!! Form::text('staus', null, ['class' => 'form-control']) !!}
+        {!! Form::text('status', null, ['class' => 'form-control']) !!}
         </div>
         
         <div class="form-group">
